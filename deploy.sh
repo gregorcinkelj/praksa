abbe711b88dfc7fbead0377612c7225b5704c33b
gregorcinkelj <50631306+gregorcinkelj@users.noreply.github.com>
2025-03-11 13:50:43 +0100

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Naloži spremenljivke iz deploy.conf
+source deploy.conf
+
+#make new directory incase there is no directory
+[ -d html ] || mkdir html
+
+#writes info  into index.html
+echo "$HTML_CONTENT" > html/index.html
+
+
+#runs docker compose
+sudo docker compose up
