ef9a1a5fb5989898d0cf9fc4ffb878106efedfe3
gregorcinkelj <50631306+gregorcinkelj@users.noreply.github.com>
2025-03-24 10:52:09 +0100

    Update deploy.sh
    added so it makes certs and conf.d if there is none

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,14 @@ source deploy.conf
 
 #make new directory incase there is no directory
 [ -d html ] || mkdir html
+[ -d certs ] || mkdir certs
+[ -d conf.d ] || mkdir conf.d
+
+#copys certs and key into certs directory
+cp nginx.crt ./certs && cp nginx.key ./certs
+
+#copy's default.conf into conf.d directory
+cp default.conf ./conf.d
 
 #writes info  into index.html
 echo "$HTML_CONTENT" > html/index.html
